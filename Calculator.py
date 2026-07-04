af3a5b5be96ff4b3236c7ef8a2ab3ccf3891ed13
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-07-04 16:30:41 +0530

    float

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+x = int(input("What's X? "))
+y = int(input("What's Y? "))
+
+print(f"The sum of {x} and {y} is {x + y}")
+print(f"The difference of {x} and {y} is {x - y}")
+print(f"The product of {x} and {y} is {x * y}")
+print(f"The quotient of {x} and {y} is {x // y}")
+print(f"The remainder of {x} and {y} is {x % y}")
+
+a = float(input("What's A? "))
+b = float(input("What's B? "))
+
+print(f"The sum of {a} and {b} is {a + b}")
+print(f"The difference of {a} and {b} is {a - b}")
+print(f"The product of {a} and {b} is {a * b}")
+print(f"The quotient of {a} and {b} is {a / b}")
+print(f"The remainder of {a} and {b} is {a % b}")
--- Hello.py
+++ Hello.py
@@ -1,3 +1,5 @@
 print("Hello, World")
-name = input("What's your Name?")
-print("Hello",name)
+name = input("What's your Name?").strip().title()
+Defo, first, last, = name.split("/")
+
+print(f"Hello {Defo}", first +f" {last}, You are the greatest person in the world")
