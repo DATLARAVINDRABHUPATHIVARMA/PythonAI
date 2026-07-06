a26110f61e94bd90aec327aaa0fa9b2143618de0
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-07-05 23:07:34 -1200

    round function syntax

--- Calculator.py
+++ Calculator.py
@@ -10,8 +10,12 @@ print(f"The remainder of {x} and {y} is {x % y}")
 a = float(input("What's A? "))
 b = float(input("What's B? "))
 
+
 print(f"The sum of {a} and {b} is {a + b}")
 print(f"The difference of {a} and {b} is {a - b}")
 print(f"The product of {a} and {b} is {a * b}")
 print(f"The quotient of {a} and {b} is {a / b}")
 print(f"The remainder of {a} and {b} is {a % b}")
+
+c = round(a + b,7)
+print(f"The sum of {a} and {b} is {c}")
