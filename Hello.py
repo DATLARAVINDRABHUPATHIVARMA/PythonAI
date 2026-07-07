63666253d6f85feaeb85232f19f7cf0fc91e328b
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-07-07 03:41:27 -0400

    Functions & Variables Module

--- Hello.py
+++ Hello.py
@@ -1,5 +1,16 @@
 print("Hello, World")
 name = input("What's your Name?").strip().title()
-Defo, first, last, = name.split("/")
+Defo, first, last = name.split("/")
 
 print(f"Hello {Defo}", first +f" {last}, You are the greatest person in the world")
+
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
+
+
+def square(n):
+    return n * n
+
+
+main()
