94491a34b9e0d461f36fa86c3ab9edc60540ea1f
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-07-27 16:01:41 +0530

    Perfect Schedule

---
+++ Loops/Mario.py
@@ -0,0 +1,15 @@
+# print("#")
+# print("#")
+# print("#")
+
+# for _ in range(7):
+#     print('#')
+
+def tab():
+    print_col(9)
+
+def print_col(c):
+    for i in range(c):
+        print("#")
+
+tab()
