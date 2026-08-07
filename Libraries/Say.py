941ae5e1934fa87ea1880471b8cfe6ec9a17948d
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-08-07 16:44:13 +0530

    Libraries

--- Libraries/Say.py
+++ Libraries/Say.py
@@ -1,6 +1,13 @@
-import cowsay
-import sys
+# import cowsay
+# import sys
 
-if len(sys.argv) == 2:
-    # cowsay.cow("Hello, " + sys.argv[1])
-    cowsay.trex("Hello, " + sys.argv[1])
+# if len(sys.argv) == 2:
+#     # cowsay.cow("Hello, " + sys.argv[1])
+#     cowsay.trex("Hello, " + sys.argv[1])
+
+import sys
+from Sayings import hello, goodbye
+
+if len(sys.argv) == 3:
+    hello(sys.argv[1])
+    goodbye(sys.argv[2])
