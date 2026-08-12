2cb7443488cccbfe18982e361ef4d1b78a8ae7af
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-08-12 12:38:14 +0530

    Lets travel in GIT

--- Libraries/Say.py
+++ Libraries/Say.py
@@ -2,7 +2,7 @@
 # import sys
 
 # if len(sys.argv) == 2:
-    cowsay.cow("Hello, " + sys.argv[1])
+    # cowsay.cow("Hello, " + sys.argv[1])
 #     cowsay.trex("Hello, " + sys.argv[1])
 
 import sys
