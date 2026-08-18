aba86494f16ac8bbd3dea8a5d6d8389c2db2ac80
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-08-18 18:25:30 +0530

    Manage

--- Libraries/Name.py
+++ Libraries/Name.py
@@ -14,8 +14,8 @@ import sys
 # else:
 #     print("Hello, my name is", sys.argv[1])
 
-if len(sys.argv) < 2:
-    sys.exit("Too few arguments")
+# if len(sys.argv) < 2:
+#     sys.exit("Too few arguments")
 elif len(sys.argv) > 2:
     print("Too many arguments")
 
