eaba9f4a47db696ca8d10b307fd93a4877b635b2
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-09-06 13:14:47 +0530

    Yoga

--- Libraries/Say.py
+++ Libraries/Say.py
@@ -1,9 +1,9 @@
 # import cowsay
 # import sys
 
-# if len(sys.argv) == 2:
-#     # cowsay.cow("Hello, " + sys.argv[1])
-#     cowsay.trex("Hello, " + sys.argv[1])
+if len(sys.argv) == 2:
+    # cowsay.cow("Hello, " + sys.argv[1])
+    cowsay.trex("Hello, " + sys.argv[1])
 
 import sys
 from Sayings import hello, goodbye
