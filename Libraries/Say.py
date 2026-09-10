c4fd62f3102aaeee2e47e853adec08a14507ad24
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-09-10 16:09:36 +0530

    Need to Work

--- Libraries/Say.py
+++ Libraries/Say.py
@@ -2,7 +2,7 @@ import cowsay
 import sys
 
 if len(sys.argv) == 2:
-    # cowsay.cow("Hello, " + sys.argv[1])
+    cowsay.cow("Hello, " + sys.argv[1])
     cowsay.trex("Hello, " + sys.argv[1])
 
 import sys
