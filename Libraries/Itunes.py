b8e16faecfc2b90341c560f19c739a1f9a2d4de4
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-09-12 16:11:00 +0530

    State Rounds

--- Libraries/Itunes.py
+++ Libraries/Itunes.py
@@ -1,11 +1,11 @@
-# import requests
-# import sys
+import requests
+import sys
 
-# if len(sys.argv) != 2:
-#     sys.exit()
+if len(sys.argv) != 2:
+    sys.exit()
 
-# response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-# print(response.json())
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+print(response.json())
 
 
 import json
@@ -16,7 +16,7 @@ if len(sys.argv) != 2:
     sys.exit()
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=100&term=" + sys.argv[1])
-# print(json.dumps(response.json(), indent=2))
+print(json.dumps(response.json(), indent=2))
 
 o = response.json()
 for result in o['results']:
