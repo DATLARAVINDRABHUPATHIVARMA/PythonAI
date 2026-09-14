8415850db333ae42d4cd760838a18a9d3e550a2e
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-09-14 16:13:30 +0530

    Python Effects

--- Libraries/Itunes.py
+++ Libraries/Itunes.py
@@ -1,14 +1,14 @@
-import requests
-import sys
+# import requests
+# import sys
 
-if len(sys.argv) != 2:
-    sys.exit()
+# if len(sys.argv) != 2:
+#     sys.exit()
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
 print(response.json())
 
 
-import jsondhvkjcfnhjz
+import json
 import requests
 import sys
 
