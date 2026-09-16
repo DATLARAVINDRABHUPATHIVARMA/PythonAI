c79a7438883fd8e8f5f1fe4b5f144e6695a3d279
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-09-16 16:23:01 +0530

    ReSchedule and Consistent

--- Conditionals/Compare.py
+++ Conditionals/Compare.py
@@ -3,28 +3,28 @@ y = int(input("Enter Y"))
 
 
 # conditional statements using if
-# if x > y:
-#     print("X is greater than Y")
-# if x < y:
-#     print("X is less than Y")
-# if x == y:
-#     print("X is equal to Y")
+if x > y:
+    print("X is greater than Y")
+if x < y:
+    print("X is less than Y")
+if x == y:
+    print("X is equal to Y")
 
 # conditional statements using if & elif
-# if x > y:
-#     print("X is greater than Y")
-# elif x < y:
-#     print("X is less than Y")
-# elif x == y:
-#     print("X is equal to Y")
+if x > y:
+    print("X is greater than Y")
+elif x < y:
+    print("X is less than Y")
+elif x == y:
+    print("X is equal to Y")
 
 # conditional statements using if, elif & else
-# if x > y:
-#     print("X is greater than Y")
-# elif x < y:
-#     print("X is less than Y")
-# else:
-#     print("X is equal to Y")
+if x > y:
+    print("X is greater than Y")
+elif x < y:
+    print("X is less than Y")
+else:
+    print("X is equal to Y")
 
 
 # conditional statements using (or) keyword
