8c77de2afe22d86f70c7ab9bd6c20fecff46ddc2
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-09-18 16:25:40 +0530

    ReSchedule and Consistent

--- Conditionals/Grade.py
+++ Conditionals/Grade.py
@@ -33,17 +33,17 @@ else:
     print("Grade: F")
 
 # Without Using "and" keyword other way
-if marks > 100 or marks < 0:
-    print("Invalid marks")
-elif 90 <= marks < 100:
-    print("Grade: A")
-elif 80 <= marks < 90:
-    print("Grade: B")
-elif 70 <= marks < 80:
-    print("Grade: C")
-elif 60 <= marks < 70:
-    print("Grade: D")
-elif 50 <= marks < 60:
-    print("Grade: E")
-else:
-    print("Grade: F")
+# if marks > 100 or marks < 0:
+#     print("Invalid marks")
+# elif 90 <= marks < 100:
+#     print("Grade: A")
+# elif 80 <= marks < 90:
+#     print("Grade: B")
+# elif 70 <= marks < 80:
+#     print("Grade: C")
+# elif 60 <= marks < 70:
+#     print("Grade: D")
+# elif 50 <= marks < 60:
+#     print("Grade: E")
+# else:
+#     print("Grade: F")
