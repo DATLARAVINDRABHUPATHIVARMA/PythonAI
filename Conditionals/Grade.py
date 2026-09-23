750fd7c18e07e670f9bc9f7efbdce170afddc13b
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-09-23 16:30:08 +0530

    Work Ethics

--- Conditionals/Grade.py
+++ Conditionals/Grade.py
@@ -39,8 +39,8 @@ elif 90 <= marks < 100:
     print("Grade: A")
 elif 80 <= marks < 90:
     print("Grade: B")
-# elif 70 <= marks < 80:
-#     print("Grade: C")
+elif 70 <= marks < 80:
+    print("Grade: C")
 # elif 60 <= marks < 70:
 #     print("Grade: D")
 # elif 50 <= marks < 60:
