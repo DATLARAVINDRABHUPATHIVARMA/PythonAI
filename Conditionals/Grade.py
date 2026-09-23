272970b0f97037d738ae6fb71f8f5b8f61569367
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-09-23 16:30:25 +0530

    Python Effects

--- Conditionals/Grade.py
+++ Conditionals/Grade.py
@@ -41,8 +41,8 @@ elif 80 <= marks < 90:
     print("Grade: B")
 elif 70 <= marks < 80:
     print("Grade: C")
-# elif 60 <= marks < 70:
-#     print("Grade: D")
+elif 60 <= marks < 70:
+    print("Grade: D")
 # elif 50 <= marks < 60:
 #     print("Grade: E")
 else:
